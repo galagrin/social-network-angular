81aa9c15cd97bf4308bee9df42df1e017235a09a
Galina Bobrik <galagrinko@gmail.com>
2025-04-30 10:19:48 +0400

    feat: add pipe for img url

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Profile } from '../../data/interfaces/profile.interface';
+import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
 
 @Component({
   selector: 'app-profile-card',
-  imports: [],
+  imports: [ImgUrlPipe],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss',
 })
